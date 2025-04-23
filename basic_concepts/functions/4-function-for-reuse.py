885d31a4bd88ed1ea3f1a8f1f4869822aee14c2a
brent just brent <belmontebrent@gmail.com>
2025-04-24 04:59:59 +0800

    4-function-for-reuse.py
    Function for Reuse:
    The function print_line() is defined to print a separator line ("------143-------"). By defining the function once, it can be reused multiple times throughout the code.
    
    Defining the Function:
    def print_line(): defines the print_line() function, which doesn't take any parameters but performs a simple task: printing a separator line.
    
    Calling the Function:
    The function print_line() is called three times. This showcases how functions can be used repeatedly in different parts of the program to avoid code duplication.

---
+++ basic_concepts/functions/4-function-for-reuse.py
@@ -0,0 +1,10 @@
+# 4.) Reusable code block to display a decorative line.
+# Function that prints a separator line
+def print_line():
+    print("------143-------")
+
+# Calls the function multiple times for formatting
+print_line()
+print("Menu")
+print_line()
+print_line()
