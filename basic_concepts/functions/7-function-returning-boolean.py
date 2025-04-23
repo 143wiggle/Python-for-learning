f15bed141d21d6564fde882cf0e14db8bb5582c5
brent just brent <belmontebrent@gmail.com>
2025-04-24 05:34:44 +0800

    7-function-returning-boolean.py
    The function is_positive(num) is defined to take one input:
    
    num: Represents the number to be evaluated.
    
    Inside the function:
    
    The function evaluates whether the number (num) is greater than 0. It returns a Boolean value (True or False).
    
    If the number is greater than 0, the function returns True (indicating the number is positive).
    
    Otherwise, it returns False (indicating the number is not positive).

---
+++ basic_concepts/functions/7-function-returning-boolean.py
@@ -0,0 +1,7 @@
+# Returns True or False based on condition
+def is_positive(num):
+    return num > 0  # Boolean return
+
+# Outputs boolean result with explanations
+print("This number is 5, therefore True for positive →", is_positive(5))
+print("This number is -3, therefore False and not positive →", is_positive(-3))
