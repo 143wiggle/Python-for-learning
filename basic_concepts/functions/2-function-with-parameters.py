f8622886d1a7671fcebb290c010f55fa22691e8e
brent just brent <belmontebrent@gmail.com>
2025-04-24 04:55:43 +0800

    2-function-with-parameters.py
    Function with Parameters:
    The function greet_person(name) accepts a parameter name. This parameter allows the function to use the passed value to customize the greeting message.
    
    Defining the Function:
    def greet_person(name): defines the function, and name is the parameter that will hold the value passed when the function is called.
    
    Calling the Function:
    greet_person("wigor") calls the function, passing the string "wigor" as the argument. The function uses this value to print "Hi wigor!".

--- basic_concepts/functions/2-function-with-parameters.py
+++ basic_concepts/functions/2-function-with-parameters.py
@@ -1,3 +1,4 @@
+# 2.) This function takes a name as input and uses it in the output.
 # Defines a function that takes a parameter called 'name'
 def greet_person(name):
     # Uses the input to customize the greeting
