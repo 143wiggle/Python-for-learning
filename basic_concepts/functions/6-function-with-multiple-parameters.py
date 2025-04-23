409d3eb6a8893bf5d1f0453f1fa876969bde5f78
brent just brent <belmontebrent@gmail.com>
2025-04-24 05:24:22 +0800

    6-function-with-multiple-parameters.py
    The function dish_name(chicken, sauce) is defined to take two inputs:
    
    chicken: Represents the food item (e.g., "Fried Chicken").
    
    sauce: Represents the type of sauce (e.g., "Hot Sauce").
    
    Inside the function:
    
    The food item (chicken) is printed with the message "The food is:".
    
    The sauce (sauce) is printed along with the message "The sauce is:" and an additional note stating that the sauce is spicy.

---
+++ basic_concepts/functions/6-function-with-multiple-parameters.py
@@ -0,0 +1,10 @@
+# Function that accepts two parameters (for Chicken & Sauce)
+def dish_name(chicken, sauce):
+    # Prints the food item
+    print("The food is:", chicken)
+    
+    # Prints the sauce and its description
+    print("The sauce is:", sauce, "and is spicy!")
+
+# Calls function with two string inputs for Fried Chicken and Hot Sauce
+dish_name("Fried Chicken", "Hot Sauce")
