d550976a502495a575664a5b6e082d7ea806b5d2
brent just brent <belmontebrent@gmail.com>
2025-04-24 04:51:56 +0800

    3-function-that-returns-a-value.py
    Function that Returns a Value:
    The function add(a, b) takes two parameters a and b, adds them together, and then returns the result. The return keyword is used to send the result back to where the function was called.
    
    Defining the Function:
    def add(a, b): defines the function add, which accepts two parameters (a and b), adds them, and returns the sum.
    
    Calling the Function:
    result = add(3, 5) calls the add() function with the values 3 and 5 as arguments. The function returns their sum (8), which is then stored in the result variable.
    
    Output:
    print(result) outputs the returned value (the sum of 3 and 5, which is 8).

---
+++ basic_concepts/functions/3-function-that-returns-a-value.py
@@ -0,0 +1,10 @@
+# 3.)Returns the result instead of printing it directly.
+# Function takes two inputs and returns their sum
+def add(a, b):
+    return a + b  # Return sends data back to where the function was called
+
+# Stores the returned result in a variable
+result = add(3, 5)
+
+# Outputs the result
+print(result)
