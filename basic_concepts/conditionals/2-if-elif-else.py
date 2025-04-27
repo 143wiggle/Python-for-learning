45779d32aee1be174381e0afac712ec51ba5d222
brent just brent <belmontebrent@gmail.com>
2025-04-28 07:34:40 +0800

    2-if-elif-else.py

---
+++ basic_concepts/conditionals/2-if-elif-else.py
@@ -0,0 +1,9 @@
+# Conditional statements example
+temperature = 30
+
+if temperature > 35:
+    print("It's really hot outside!")
+elif temperature > 20:
+    print("Nice and warm.")
+else:
+    print("It's a bit chilly.")
