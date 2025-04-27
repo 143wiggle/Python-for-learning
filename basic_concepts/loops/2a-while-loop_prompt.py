0c4a1c2fa7e72f3be0a4a5cbfb37a7540854e498
brent just brent <belmontebrent@gmail.com>
2025-04-28 07:37:00 +0800

    2a-while-loop_prompt.py

---
+++ basic_concepts/loops/2a-while-loop_prompt.py
@@ -0,0 +1,6 @@
+# A while loop with user input
+counter = 0
+max_count = int(input("How many times would you like to loop? "))
+while counter < max_count:
+    print("Counter:", counter)
+    counter += 1
