4f3918d5b4e4a9d3fbe3e76cb46d4acd7fafd1e9
brent just brent <belmontebrent@gmail.com>
2025-04-28 09:59:21 +0800

    4-lists.py

---
+++ basic_concepts/data_structures/4-lists.py
@@ -0,0 +1,13 @@
+# A list in Python: ordered collection of items
+fruits = ["apple", "banana", "cherry", "date"]
+
+# Accessing elements
+print(f"First fruit: {fruits[0]}")
+
+# Adding and removing elements
+fruits.append("elderberry")
+fruits.remove("banana")
+
+# Iterating over a list
+for fruit in fruits:
+    print(fruit)
