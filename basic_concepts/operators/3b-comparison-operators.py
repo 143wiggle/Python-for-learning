364dca57438c81685301ac256e335422a0fbc83e
brent just brent <belmontebrent@gmail.com>
2025-04-28 09:55:42 +0800

    3b-comparison-operators.py

---
+++ basic_concepts/operators/3b-comparison-operators.py
@@ -0,0 +1,10 @@
+# Comparison operators: ==, !=, >, <, >=, <=
+a = 10
+b = 20
+
+print(f"Is a equal to b? {a == b}")
+print(f"Is a not equal to b? {a != b}")
+print(f"Is a greater than b? {a > b}")
+print(f"Is a less than b? {a < b}")
+print(f"Is a greater than or equal to b? {a >= b}")
+print(f"Is a less than or equal to b? {a <= b}")
