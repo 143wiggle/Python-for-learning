0e818e379e8b80ed35aa0121fa0df73aadc2e81f
brent just brent <belmontebrent@gmail.com>
2025-04-28 09:57:40 +0800

    3d-logical-operators.py

---
+++ basic_concepts/operators/3d-logical-operators.py
@@ -0,0 +1,7 @@
+# Logical operators: and, or, not
+x = 5
+y = 10
+
+print(f"Is x > 3 and y < 15? {x > 3 and y < 15}")
+print(f"Is x < 3 or y > 5? {x < 3 or y > 5}")
+print(f"Is x not equal to 5? {not x == 5}")
