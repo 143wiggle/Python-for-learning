b0d6598cad941dddf9befea27cd279f710f2ee63
brent just brent <belmontebrent@gmail.com>
2025-04-28 10:07:39 +0800

    6-strings.py

---
+++ basic_concepts/strings/6-strings.py
@@ -0,0 +1,11 @@
+# String operations
+name = "Alice"
+greeting = "Hello, " + name
+
+print(greeting)
+
+# String methods
+message = "Python is great!"
+print(message.upper())
+print(message.lower())
+print(message.replace("great", "awesome"))
